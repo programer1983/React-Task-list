90b3e327cc28b429bc528ca0da8f44d965fbfa28
programer1983 <programer972@gmail.com>
2022-04-04 15:51:59 +0300

    commit-5;

--- src/App.js
+++ src/App.js
@@ -24,9 +24,13 @@ function App() {
       <header>
         <h1>Список Задач: {todos.length}</h1>
       </header>
-      <ToDoForm />
+      <ToDoForm addTask={addTask} />
       {todos.map((todo) => {
-        <ToDo key={todo.id} />
+        <ToDo 
+          key={todo.id} 
+          toggleTask={handleToggle} 
+          removeTask={removeTask} 
+        />
       })}
     </div>
   );
