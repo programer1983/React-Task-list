72fef978968cd78c032de201f1f7297914f7dd2d
programer1983 <programer972@gmail.com>
2022-04-04 16:06:12 +0300

    commit-6;

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,6 +1,35 @@
+import {useState} from "react"
+
 
 function ToDoForm () {
-  return null
+    const [userInput, setUserInput] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const handleChange = () => {
+
+    }
+
+    const handleKeyPress = () => {
+
+    }
+
+
+    
+  return (
+      <form onSubmit={handleSubmit}>
+        <input 
+            type="text" 
+            value={userInput} 
+            onChange={handleChange}
+            onKeyDown={handleKeyPress}
+            placeholder="Введите значения..."
+        />
+        <button>Сохранить</button>
+      </form>
+  )
 }
 
 export default ToDoForm
