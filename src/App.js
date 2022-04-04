1c4a667ae6e2e29f06d68b7953131aca475212dd
programer1983 <programer972@gmail.com>
2022-04-04 15:47:05 +0300

    commit-4;

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import ToDoForm from "./components/ToDoForm";
 
 
 function App() {
+  const [todos, setToDos] = useState([])
+
+  const addTask = () => {
+
+  }
+
+  const removeTask = () => {
+    
+  }
+
+  const handleToggle = () => {
+    
+  }
+
+
   return (
     <div className="App">
-     
+      <header>
+        <h1>Список Задач: {todos.length}</h1>
+      </header>
+      <ToDoForm />
+      {todos.map((todo) => {
+        <ToDo key={todo.id} />
+      })}
     </div>
   );
 }
